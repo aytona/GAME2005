c3101200745e1383003239687b319d488d92535b
Christopher Aytona <christopher.aytona@hotmail.com>
2015-09-26 20:19:59 -0400

    Week 3 Lab added

---
+++ Labs/Week3/Assets/GetAcceleration.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GetAcceleration : MonoBehaviour {
+
+	private Vector3 lastPosition;
+	private float Velocity = 0;
+	private float Acceleration = 0;
+	Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		Velocity = (transform.position.z - lastPosition.z) / Time.deltaTime;
+		lastPosition.z = transform.position.z;
+		Acceleration = Velocity / Time.deltaTime;
+
+		Debug.Log (Acceleration);
+		//Debug.Log (Velocity);
+		//Debug.Log (rb.velocity.z);
+	}
+}
