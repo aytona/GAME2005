bc3861998eea2e21269ac9c29d1e3358f43196ed
aytona <christopher.aytona@gmail.com>
2015-10-03 23:27:16 -0400

    Lab 4 Added

---
+++ Labs/Lab4/Assets/NewBehaviourScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class NewBehaviourScript : MonoBehaviour {
+
+	Rigidbody rb;
+
+	public float angle = 30f;
+	public float initialVelocity;
+	public float velocity;
+	
+	private float acceleration;
+	private float gravity = -9.8f;
+	private float distance = 10f;
+	private float friction;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		Calculate ();
+		rb.AddForce (Vector3.left * initialVelocity);
+	}
+
+	void FixedUpdate()
+	{
+		//velocity = (transform.position.x - lastPosition.x) / Time.deltaTime;
+		//rb.AddForce (Vector3.left * initialVelocity);
+		//Debug.Log (rb.velocity.normalized.x);
+		//rb.velocity = initialVelocity;
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Hole")
+			acceleration = 0;
+	}
+
+	void Calculate()
+	{
+		friction = rb.mass * gravity * Mathf.Cos (Mathf.Rad2Deg * angle) * 0.6f;
+		acceleration = gravity * (Mathf.Sin (Mathf.Rad2Deg * angle)) + friction * rb.mass;
+		Debug.Log (acceleration);
+		initialVelocity = Mathf.Sqrt (2 * acceleration * distance);
+		Debug.Log (initialVelocity);
+	}
+}
