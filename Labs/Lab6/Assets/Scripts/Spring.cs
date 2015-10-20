7a46a66132ca4123f5d4ba5b043a254190573967
aytona <christopher.aytona@gmail.com>
2015-10-19 23:38:34 -0400

    Lab 6 Added

---
+++ Labs/Lab6/Assets/Scripts/Spring.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour {
+
+	Rigidbody objectMass;
+
+	public float constant;
+	public float compression;
+
+	private float horizontalAngle = 45f;
+	private float verticalAngle = 90f;
+	private float mass;
+
+	void Start()
+	{
+		objectMass = GetComponent<Rigidbody> ();
+		mass = objectMass.mass;
+		Vector3 angleVector = new Vector3 (Mathf.Cos(horizontalAngle), Mathf.Sin(verticalAngle), 0.0f);
+		angleVector.Normalize ();
+		Vector3 springVelocity = angleVector * (compression * Mathf.Sqrt (constant / mass));
+		objectMass.AddForce (springVelocity);
+	}
+}
---
+++ Labs/Lab6/Assets/Scripts/Wind.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour {
+
+	public float WindPower;
+
+	void OnTriggerStay(Collider other)
+	{
+		other.GetComponent<Rigidbody> ().AddForce (Vector3.forward * WindPower);
+	}
+}
