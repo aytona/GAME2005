828c18d3b2ef500d686938e56b119b50ac65b76b
Christopher Aytona <christopher.aytona@hotmail.com>
2015-11-06 14:05:20 -0500

    Lab 9 added

---
+++ Labs/Lab9/Assets/Scripts/addVelocity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class addVelocity : MonoBehaviour {
+
+    public float thrust;
+
+    private Rigidbody rb;
+
+	void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.right * thrust);
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("Small Sphere:" + rb.velocity);
+    }
+}
---
+++ Labs/Lab9/Assets/Scripts/readVelocity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class readVelocity : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate()
+    {
+        Debug.Log("Big Sphere: " + rb.velocity);
+    }
+}
